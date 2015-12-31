f47994db4f53bd6f5db3bb4a7bace507dfd02288
katjam <katja@neontribe.co.uk>
2015-12-31 14:08:48 +0000

    strip formatting on scn index

--- resources/views/scenes/index.blade.php
+++ resources/views/scenes/index.blade.php
@@ -3,9 +3,17 @@
 <h1>Scenes</h1>
   <ul class="strips">
   @foreach ($scenes as $scene)
-    <li>{{ $scene->scn_no }} {{ $scene->int_ext }} {{ $scene->setting->set_name }} - {{ $scene->day_night }} {{ $scene->page_eights() }}
+    <li><span class=" boxed scn-no">{{ $scene->scn_no }}</span>
+      <span class="set-desc boxed">
+        <span class="setting">{{ $scene->int_ext }}. {{ $scene->setting->set_name }} - {{ $scene->day_night }}</span>
+        <span class="description">{{ $scene->description }}</span>
+      </span>
+      <span class="eights boxed">
+        {{ $scene->page_eights() }}
+      </span>
+      <span class="characters">@foreach ($scene->characters as $cast) {{ $cast->character_name }}, @endforeach </span>
+
     <span class="no-print"><a href ="scenes/{{ $scene->id }}">view</a> <a href="scenes/ {{ $scene->id }}/edit">edit</a></span>
-      <ul><div>@foreach ($scene->characters as $cast)<li> {{ $cast->character_name }}</li> @endforeach </div></ul>
     </li>
   @endforeach
   </ul>
