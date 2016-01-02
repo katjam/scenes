9ad6c56cf720b95cf7f2c2adc8ac92fd010d4f5e
katjam <katja@neontribe.co.uk>
2016-01-02 16:24:50 +0000

    better strip styles

--- resources/views/scenes/index.blade.php
+++ resources/views/scenes/index.blade.php
@@ -3,17 +3,19 @@
 <h1>Scenes</h1>
   <ul class="strips">
   @foreach ($scenes as $scene)
-    <li><span class=" boxed scn-no">{{ $scene->scn_no }}</span>
-      <span class="set-desc boxed">
+    <li>
+      <span class="no-print"><a href ="scenes/{{ $scene->id }}">view</a> <a href="scenes/ {{ $scene->id }}/edit">edit</a></span>
+
+      <span class="scn-no">{{ $scene->scn_no }}</span>
+      <span class="set-desc">
         <span class="setting">{{ $scene->int_ext }}. {{ $scene->setting->set_name }} - {{ $scene->day_night }}</span>
         <span class="description">{{ $scene->description }}</span>
       </span>
-      <span class="eights boxed">
+      <span class="eights">
         {{ $scene->page_eights() }}
       </span>
       <span class="characters">@foreach ($scene->characters as $cast) {{ $cast->character_name }}, @endforeach </span>
 
-    <span class="no-print"><a href ="scenes/{{ $scene->id }}">view</a> <a href="scenes/ {{ $scene->id }}/edit">edit</a></span>
     </li>
   @endforeach
   </ul>
