ba0055e4d540143c7158fe8445dfe674503c9d84
katjam <katja@neontribe.co.uk>
2016-02-07 12:17:56 +0000

    closes #10 Re-order fields and better scene edit forms

--- resources/views/scenes/create.blade.php
+++ resources/views/scenes/create.blade.php
@@ -6,51 +6,49 @@
     <!-- scn number -->
     {!! Form::label('scn_no', 'Scene Number') !!}
     {!! Form::text('scn_no') !!}
+    <label>Setting</label>
+    {!! Form::select('setting_id', $settings) !!}
+    <!-- int ext -->
+    <div class="radio">
+      {!! Form::label('int_ext', 'Int') !!}
+      {!! Form::radio('int_ext', 'INT', false) !!}
+    </div>
+    <div class="radio">
+      {!! Form::label('int_ext', 'Ext') !!}
+      {!! Form::radio('int_ext', 'EXT', false) !!}
+    </div>
+     <!--  day night -->
+    <div class="radio">
+      {!! Form::label('day_night', 'Day') !!}
+      {!! Form::radio('day_night', 'day', false) !!}
+    </div>
+    <div class="radio">
+      {!! Form::label('day_night', 'Night') !!}
+      {!! Form::radio('day_night', 'night', false) !!}
+    </div>
+    <div class="radio">
+      {!! Form::label('day_night', 'Dawn') !!}
+      {!! Form::radio('day_night', 'dawn', false) !!}
+    </div>
+    <div class="radio">
+      {!! Form::label('day_night', 'Dusk') !!}
+      {!! Form::radio('day_night', 'dusk', false) !!}
+    </div>
+
+    <!-- page count -->
+    {!! Form::label('page_count', 'Page Count Eights') !!}
+    {!! Form::number('page_count', '') !!} /8
+
+    <label>Characters</label>
+    @foreach ($characters as $character)
+    <div>{!! Form::checkbox('character_id[]', $character->id) !!} {!! $character->character_name !!}</div>
+    @endforeach
 
     <!-- description -->
     {!! Form::label('description', 'Description') !!}
     {!! Form::textarea('description') !!}
 
-    <!-- int ext -->
-    <h3>INT/EXT</h3>
-    <div>
-      {!! Form::label('int_ext', 'Int') !!}
-      {!! Form::radio('int_ext', 'INT', false) !!}
-    </div>
-    <div>
-      {!! Form::label('int_ext', 'ext') !!}
-      {!! Form::radio('int_ext', 'EXT', false) !!}
-    </div>
-    <h3>Setting</h3>
-    {!! Form::select('setting_id', $settings) !!}
-
-     <!--  day night -->
-    <h3>Day/Night</h3>
-    <div>
-      {!! Form::label('day_night', 'Day') !!}
-      {!! Form::radio('day_night', 'day', false) !!}
-    </div>
-    <div>
-      {!! Form::label('day_night', 'Night') !!}
-      {!! Form::radio('day_night', 'night', false) !!}
-    </div>
-    <div>
-      {!! Form::label('day_night', 'Dawn') !!}
-      {!! Form::radio('day_night', 'dawn', false) !!}
-    </div>
-    <div>
-      {!! Form::label('day_night', 'Dusk') !!}
-      {!! Form::radio('day_night', 'dusk', false) !!}
-    </div>
-    <h3>Characters</h3>
-    @foreach ($characters as $character)
-    <div>{!! Form::checkbox('character_id[]', $character->id) !!} {!! $character->character_name !!}</div>
-    @endforeach
-
-   <!-- page count -->
-    {!! Form::label('page_count', 'Page Count Eights') !!}
-    {!! Form::number('page_count', '') !!} /8
-   <!-- filming day -->
+    <!-- filming day -->
     {!! Form::label('filming_day', 'Filming Day') !!}
     {!! Form::number('filming_day') !!}
 
