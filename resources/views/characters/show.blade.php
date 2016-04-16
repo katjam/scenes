933fdf6b228c51644d12e69da596f45971575441
katjam <katja@neontribe.co.uk>
2016-04-16 16:26:41 +0100

    By char listing in strip style with set name part of #16 closes #28

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -1,15 +1,23 @@
 @extends('html')
 @section('content')
     <p>Total Page: {{ $page_count }}</p>
-    <p>{{ $character->character_name }}</p>
+    <h1>{{ $character->character_name }}</h1>
     <p>{{ $character->cast_type }}</p>
     <p>{{ $character->description }}</p>
     <p>{{ $character->actor }}</p>
     <p>{{ $character->contact }}</p>
-    @foreach ($character->scenes as $scn)
-      <div>
-      <div>{{ $scn->scn_no }} - {{ $scn->int_ext }} - {{ $scn->day_night }}</div>
-      <div>{{ $scn->description }}</div>
-      </div>
-    @endforeach
+    <ul class="strips">
+        @foreach ($character->scenes as $scn)
+        <li>
+            <span class="scn-no">{{ $scn->scn_no }}</span>
+            <span class="set-desc">
+                <span class="setting">{{ $scn->int_ext }}. {{ $scn->setting->set_name }} - {{ $scn->day_night }}</span>
+                <span class="description">{{ $scn->description }}</span>
+            </span>
+            <span class="eights">
+                {{ $scn->page_eights() }}
+            </span>
+        </li>
+        @endforeach
+    </ul>
 @endsection
