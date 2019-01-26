8ecfd0c7251cc53aaae48ac763596b14bf4895f8
katjam <katja@neontribe.co.uk>
2019-01-26 12:54:52 +0000

    Upgrade to 5.2

--- config/app.php
+++ config/app.php
@@ -16,6 +16,14 @@ return [
     'debug' => env('APP_DEBUG', false),
 
     /*
+    |---
+    | env var added in 5.2 upgrade
+    |---
+    |
+    */
+    'env' => env('APP_ENV', 'local'),
+
+    /*
     |--------------------------------------------------------------------------
     | Application URL
     |--------------------------------------------------------------------------
@@ -113,13 +121,11 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
-        Illuminate\Foundation\Providers\ArtisanServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
