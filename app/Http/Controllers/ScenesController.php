b8b549875ab7c6d3f9466e74571ca0d5c33b805b
katjam <katja@neontribe.co.uk>
2019-01-27 13:21:08 +0000

    Higher sub scene numbers allowed

--- app/Http/Controllers/ScenesController.php
+++ app/Http/Controllers/ScenesController.php
@@ -24,23 +24,41 @@ class ScenesController extends Controller
 			$sort_scenes = Scene::all()->sortBy(function ($scene, $key) {
 				// todo order by subscenes (a, b, c). Currently they appear before original
 				if (preg_match('/a$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.1;
+				  return intval($scene['scn_no']) + 0.01;
 				}
 				if (preg_match('/b$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.2;
+				  return intval($scene['scn_no']) + 0.02;
 				}
 			  if (preg_match('/c$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.3;
+				  return intval($scene['scn_no']) + 0.03;
 				}
 			  if (preg_match('/d$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.4;
+				  return intval($scene['scn_no']) + 0.04;
 				}
 			  if (preg_match('/e$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.5;
-              }
-              if (preg_match('/f$/', $scene['scn_no'])) {
-				  return intval($scene['scn_no']) + 0.6;
-              } else {
+				  return intval($scene['scn_no']) + 0.05;
+        }
+        if (preg_match('/f$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.06;
+        }
+        if (preg_match('/g$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.07;
+        }
+        if (preg_match('/h$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.08;
+        }
+        if (preg_match('/i$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.09;
+        }
+        if (preg_match('/j$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.1;
+        }
+        if (preg_match('/k$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.2;
+        }
+        if (preg_match('/l$/', $scene['scn_no'])) {
+				  return intval($scene['scn_no']) + 0.3;
+        } else {
 					return intval($scene['scn_no']);
 			  }
 			});
@@ -50,8 +68,9 @@ class ScenesController extends Controller
 			case('location'):
 			  $scenes = $sort_scenes->groupBy((function ($item, $key) { $setting =  Setting::find($item['setting_id']); return $setting->location . ' ' . $setting->set_name; }))->sortBy(function ($item, $key) { return $key; });
 				break;
-            case('shoot day'):
-                $scenes = Scene::all()->sortBy('setting_id')->groupBy('filming_day')->sortBy(function ($item, $key) { return intval($key); });
+      case('shoot day'):
+        // Todo order by a,b,c subscenes
+        $scenes = Scene::all()->sortBy('setting_id')->groupBy('filming_day')->sortBy(function ($item, $key) { return intval($key); });
 				break;
 			case('story'):
 			default:
